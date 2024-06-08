8a8c585b4c5a12df8ee80fe090c82f11f6b9cbdc
Taha HALIM <107689251+Tatsuhik-o@users.noreply.github.com>
2024-06-08 17:23:07 +0100

    modified files structures to fix a github issue that fails to pull images

--- js/main.js
+++ js/main.js
@@ -45,6 +45,7 @@ async function addNewUserEntry(currentActive){
 
         newUserEntry.append(newUserMessage, userIcon)
         chatRoom.append(newUserEntry)
+        //AI
         messageArea.value = ''
         localStorage.setItem(currentActive, chatRoom.innerHTML)
     }
@@ -114,13 +115,67 @@ const currentActiveConversations = document.querySelectorAll('.newConvo')
 currentActiveConversations.forEach((elem, idx) => {
     elem.addEventListener('click', () => {
         elem.classList.add('active')
-        userPicture.style.backgroundImage = `url('../public/img/client${idx+1}.png')`
+        resetClassList()
+        userPicture.classList.add(returnImage(idx))
         currentActiveConversations[currentActive].classList.remove('active')
         currentActive = idx
         chatRoom.innerHTML = localStorage.getItem(currentActive)
     })
 })
-console.log(localStorage)
 window.addEventListener('load', () => {
+    if(localStorage.getItem(currentActive)){
+        currentActive = parseInt(localStorage.getItem('currentActive'))
+    }
+    else{
+        currentActive = 0
+    }
     chatRoom.innerHTML = localStorage.getItem(currentActive)
+    currentActiveConversations[currentActive].classList.add('active')
+    resetClassList()
+    userPicture.classList.add(returnImage(currentActive))
 })
+function returnImage(num){
+    if(num === 0) return 'userOne' 
+    if(num === 1) return 'userTwo' 
+    if(num === 2) return 'userThree' 
+    if(num === 3) return 'userFour' 
+    if(num === 4) return 'userFive' 
+}
+function resetClassList(){
+    userPicture.classList.remove('userOne')
+    userPicture.classList.remove('userTwo')
+    userPicture.classList.remove('userThree')
+    userPicture.classList.remove('userFour')
+    userPicture.classList.remove('userFive')
+}
+window.addEventListener('beforeunload', () => {
+    localStorage.setItem('currentActive', currentActive)
+})
+async function AI_response(message, emotion){
+    const API_KEY = '';
+    const sentData = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+        { role: 'system', content: `keep in mind that user is ${emotion}, so say something appropriate to his or her emotions` },
+        { role: 'user', content: `${message}` }
+    ],
+    max_tokens: 200
+    };
+    const options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${API_KEY}`
+                    },
+                    body: JSON.stringify(sentData)
+                    }
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+    const AI_Message = await response.json()
+    console.log(AI_Message)
+}
+/*
+function getEmotion(text_Message){
+
+}
+*/
+//AI_response('My name is Taha, very pleased to meet you', 'Happy')
